6bbbf94d8f09ee34010bebe52828a439a1d9f995
Maksim Levental <maksim.levental@gmail.com>
2025-01-06 17:05:41 -0500

    bind correctly

--- python/TritonExtension.cpp
+++ python/TritonExtension.cpp
@@ -12,25 +12,7 @@
 using namespace mlir::python::adaptors;
 
 PYBIND11_MODULE(_site_initialize_0, m) {
-  //===--------------------------------------------------------------------===//
-  // triton dialect
-  //===--------------------------------------------------------------------===//
-  m.def(
-      "register_dialect",
-      [](MlirContext context, bool load) {
-        MlirDialectHandle handle_triton = mlirGetDialectHandle__triton__();
-        MlirDialectHandle handle_triton_gpu =
-            mlirGetDialectHandle__triton_gpu__();
-        MlirDialectHandle handle_triton_amd_gpu =
-            mlirGetDialectHandle__triton_amd_gpu__();
-        mlirDialectHandleRegisterDialect(handle_triton, context);
-        mlirDialectHandleRegisterDialect(handle_triton_gpu, context);
-        mlirDialectHandleRegisterDialect(handle_triton_amd_gpu, context);
-        if (load) {
-          mlirDialectHandleLoadDialect(handle_triton, context);
-          mlirDialectHandleLoadDialect(handle_triton_gpu, context);
-          mlirDialectHandleLoadDialect(handle_triton_amd_gpu, context);
-        }
-      },
-      py::arg("context") = py::none(), py::arg("load") = true);
+  m.def("register_dialects", [](MlirDialectRegistry registry) {
+    tritonMlirRegisterTritonDialectsAndPasses(registry);
+  });
 }
