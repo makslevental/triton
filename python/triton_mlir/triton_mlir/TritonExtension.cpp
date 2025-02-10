8391d7e2fd94afb148b296694cf9f9b86ca5e13b
Maksim Levental <maksim.levental@gmail.com>
2025-02-10 13:36:50 -0500

    continue runtime

--- python/triton_mlir/triton_mlir/TritonExtension.cpp
+++ python/triton_mlir/triton_mlir/TritonExtension.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir-c/Target/LLVMIR.h"
 #include "mlir/Bindings/Python/NanobindAdaptors.h"
 #include "triton-c/Dialects.h"
 
@@ -13,9 +14,25 @@
 
 using namespace mlir::python;
 using namespace mlir::python::nanobind_adaptors;
-using namespace nanobind::literals;
+namespace nb = nanobind;
+using namespace nb::literals;
+
+static const std::string kNanobindCapsuleAttr = "ptr";
+static const std::string kNanobindCapsuleName = "nb_handle";
+static const std::string kEUDSLLLVMPyImportName = "llvm.eudslllvm_ext";
+static const std::string kEUDSLLLVMPyFromCapsuleAttr = "from_capsule";
+
+static LLVMContextRef
+LLVMContextRefFromApiObject(const nb::object &llvmCtxApiObject) {
+  nb::capsule obj = llvmCtxApiObject.attr(kNanobindCapsuleAttr.c_str());
+  void *ptr = PyCapsule_GetPointer(obj.ptr(), kNanobindCapsuleName.c_str());
+  auto ctx = static_cast<LLVMContextRef>(ptr);
+  return ctx;
+}
 
 NB_MODULE(_TritonExtension, m) {
+  nb::set_leak_warnings(false);
+
   m.def("get_ptr_type_typeid",
         []() { return tritonMlirPointerTypeGetTypeID(); });
   mlir_type_subclass(m, "PointerType", tritonMlirTypeIsAPointerType,
@@ -35,4 +52,21 @@ NB_MODULE(_TritonExtension, m) {
       .def_property_readonly("pointee_type", [](MlirType self) {
         return tritonMlirPointerTypeGetPointeeType(self);
       });
+
+  m.def("has_matrix_core_feature", [](const std::string &arch) {
+    MlirStringRef a = mlirStringRefCreate(arch.data(), arch.size());
+    return hasMatrixCoreFeature(a);
+  });
+
+  m.def("translate_to_llvmir",
+        [](MlirModule module, const nb::object &llvmCtxApiObject) {
+          MlirOperation operation = mlirModuleGetOperation(module);
+          auto mod = mlirTranslateModuleToLLVMIR(
+              operation, LLVMContextRefFromApiObject(llvmCtxApiObject));
+          return nanobind::module_::import_(kEUDSLLLVMPyImportName.c_str())
+              .attr("ModuleRef")
+              .attr(kEUDSLLLVMPyFromCapsuleAttr.c_str())(
+                  nb::capsule(mod, kNanobindCapsuleName.c_str()))
+              .release();
+        });
 }
