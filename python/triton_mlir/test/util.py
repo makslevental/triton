844374177b938e05468f78f80784ebd60bfe524a
Maksim Levental <maksim.levental@gmail.com>
2025-02-15 11:52:58 -0500

    setup.py extras

--- python/triton_mlir/test/util.py
+++ python/triton_mlir/test/util.py
@@ -1,10 +1,25 @@
+def hip_check(call_result):
+    import hip
+
+    err = call_result[0]
+    result = call_result[1:]
+    if len(result) == 1:
+        result = result[0]
+    if isinstance(err, hip.hipError_t) and err != hip.hipError_t.hipSuccess:
+        raise RuntimeError(str(err))
+    return result
+
+
 def hip_bindings_not_installed():
     try:
         import hip
 
+        props = hip.hipDeviceProp_t()
+        hip_check(hip.hipGetDeviceProperties(props, 0))
+
         # don't skip
         return False
 
-    except ImportError:
+    except:
         # skip
         return True
