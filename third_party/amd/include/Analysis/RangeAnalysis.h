987cfd651cf092714dc917fa07f52a9b8dff19ec
Maksim Levental <maksim.levental@gmail.com>
2025-03-28 15:21:50 -0400

    add tests

--- third_party/amd/include/Analysis/RangeAnalysis.h
+++ third_party/amd/include/Analysis/RangeAnalysis.h
@@ -121,8 +121,8 @@ std::optional<SmallVector<ConstantIntRanges>>
 collectRanges(const DataFlowSolver &solver, ValueRange values);
 bool cmpIIsStaticallyTrue(const DataFlowSolver &solver, arith::CmpIOp cmpOp);
 
-void populateFoldTrueCmpIOpPatterns(
-    RewritePatternSet &patterns, std::shared_ptr<DataFlowSolver> solver);
+void populateFoldTrueCmpIOpPatterns(RewritePatternSet &patterns,
+                                    std::shared_ptr<DataFlowSolver> solver);
 
 } // namespace mlir::triton::AMD
 
